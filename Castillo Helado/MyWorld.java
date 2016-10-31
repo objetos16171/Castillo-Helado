20a1d35b56e267bda646fc91d18794f0c7135a8f
EmmanuelEscobar <emmachikane@gmail.com>
2016-10-31 09:54:25 -0600

    Lluvia de Pinguinos
    Se esta haciendo las pruebas y la primera version del metodo
    LluviaPinguino

--- Castillo Helado/MyWorld.java
+++ Castillo Helado/MyWorld.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    private Pinguino pinguino;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -17,5 +18,18 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+    }
+    
+    public void LluviaPinguino()
+    {
+        pinguino = new Pinguino();
+        for(int cont=0;cont<5;cont++){
+        addObject(pinguino,Greenfoot.getRandomNumber(800),Greenfoot.getRandomNumber(50) );
+        }
+    }
+    
+    public void act()
+    {
+        LluviaPinguino();
     }
 }
