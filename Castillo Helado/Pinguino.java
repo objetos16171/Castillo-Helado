3170ed6dc8e86037166aeea6262025998a0f5994
EmmanuelEscobar <emmachikane@gmail.com>
2016-11-03 13:14:37 -0600

    Animacion Pinguino
    Se añadio la ubicacion del archivo gif para la clase Pinguino

--- Castillo Helado/Pinguino.java
+++ Castillo Helado/Pinguino.java
@@ -8,12 +8,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Pinguino extends Actor
 {
+    
+    GifImage gifImage = new GifImage("pinguinoMejorado2.gif");
     /**
      * Act - do whatever the Pinguino wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
+        setImage(gifImage.getCurrentImage());
         // Add your action code here.
     }    
 }
