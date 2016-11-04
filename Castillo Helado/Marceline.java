1d4fbb42043c08ba6ef89f1a3315ad801580dcb1
EmmanuelEscobar <emmachikane@gmail.com>
2016-11-04 00:07:41 -0600

    Animacion Clase Marceline
    Se le agrego el archivo gif a la clase marceline

--- Castillo Helado/Marceline.java
+++ Castillo Helado/Marceline.java
@@ -8,12 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Marceline extends Actor
 {
+    
+    private GifImage gifMarceline= new GifImage("marcelineCanta.gif");
+    
     /**
      * Act - do whatever the Marceline wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
+        setImage(gifMarceline.getCurrentImage());
         // Add your action code here.
     }    
 }
