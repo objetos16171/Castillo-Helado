7c7747dbeee9981f873804c2e1e7096973af4e98
carosoto <krooo95@hotmail.com>
2016-11-06 14:52:53 -0600

    princesas

---
+++ Castillo Helado/princesaEsqueleto.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class princesaEsqueleto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class princesaEsqueleto extends Actor
+{
+    /**
+     * Act - do whatever the princesaEsqueleto wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
